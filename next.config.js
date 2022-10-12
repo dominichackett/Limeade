5864e900ca07a3c9ba380f9a72d22aef9ed3d6ed
Felix <dexdaprotocol@gmail.com>
2022-10-13 00:59:10 +0200

    INIT
    INIT

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["ipfs.moralis.io"],
+  },
+  env: {
+    NEXT_APP_MORALIS_SERVER_URL: "",
+    NEXT_APP_MORALIS_APP_ID: "",
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import Landing from "../src/Components/Landing";
 
 const Home: NextPage = () => {
+  const [auth, setAuth] = useState(false);
+
+  if (!auth) return <Landing />;
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -12,14 +17,14 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
+          Welcome to{" "}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
             pages/index.tsx
           </code>
@@ -75,12 +80,12 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
