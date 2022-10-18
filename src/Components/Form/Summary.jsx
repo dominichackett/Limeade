b154a18efefaff42e9f6a7038d6ad0e188871e94
Felix <dexdaprotocol@gmail.com>
2022-10-18 23:32:52 +0200

    Update Summary.jsx

--- src/Components/Form/Summary.jsx
+++ src/Components/Form/Summary.jsx
@@ -8,29 +8,31 @@ export default function Summary(props) {
       <h1 className="text-4xl tracking-widest absolute left-80 top-52 whitespace-nowrap">
         {props.title}
       </h1>
-      <div className="mt-6 flex  flex-col items-center h-80 w-full justify-around py-8 bg-[#CAF46F] bg-opacity-70 rounded-xl sm:w-full">
+      <div className="mt-6 flex  flex-col items-center h-96 w-full justify-around py-8 bg-[#CAF46F] bg-opacity-70 rounded-xl sm:w-full">
         <h3 className="text-3xl font-bold">
           Here's a summary of {props.petName}'s information
         </h3>
-        <div className="flex flex-col items-center justify-start ">
-          <p>Name</p>
-          <p>{props.petName}</p>
-        </div>
-        <div className="flex flex-col items-center justify-start ">
-          <p>Gender</p>
-          <p>{props.petGender}</p>
-        </div>
-        <div className="flex flex-col items-center justify-start ">
-          <p>Age</p>
-          <p>{props.petAge} Years old</p>
-        </div>
-        <div className="flex flex-col items-center justify-start ">
-          <p>Breed</p>
-          <p>{props.petBreed}</p>
-        </div>
-        <div className="flex flex-col items-center justify-start ">
-          <p>Medical History</p>
-          <p>{props.medicalHistory}</p>
+        <div className="grid grid-cols-3 gap-4 w-full">
+          <div className="flex flex-col items-center justify-start ">
+            <p className="text-xl">Name</p>
+            <p className="text-base font-bold">{props.petName}</p>
+          </div>
+          <div className="flex flex-col items-center justify-start ">
+            <p className="text-xl">Gender</p>
+            <p className="text-base font-bold">{props.petGender}</p>
+          </div>
+          <div className="flex flex-col items-center justify-start ">
+            <p className="text-xl">Age</p>
+            <p className="text-base font-bold">{props.petAge} Years old</p>
+          </div>
+          <div className="flex flex-col items-center justify-start ">
+            <p className="text-xl">Breed</p>
+            <p className="text-base font-bold">{props.petBreed}</p>
+          </div>
+          <div className="flex flex-col items-center justify-start ">
+            <p className="text-xl">Medical History</p>
+            <p className="text-base font-bold">{props.medicalHistory}</p>
+          </div>
         </div>
 
         <button
