2dc140fd0187c4b824cab41b8d72db70ea1c003a
Dominic Leon Hackett <dominichackett@gmail.com>
2022-11-10 15:16:42 -0400

    Claim Validation Errors Vercel
    Fixed Claim Validation error

--- src/Components/Agency/ClaimValidation.jsx
+++ src/Components/Agency/ClaimValidation.jsx
@@ -29,7 +29,7 @@ export default function ClaimValidation(props) {
               Type
             </dt>
             <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              {router.query.petType.toUpperCase()} - {router.query.gender.toUpperCase()} - {router.query.breed.toUpperCase()} 
+            {(router.query?.petType ? router.query?.petType.toUpperCase() : "")} - {(router.query?.gender ? router.query?.gender.toUpperCase(): "")} - {(router.query?.breed ? router.query?.breed?.toUpperCase() : "")} 
             </dd>
           </div>
           <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
